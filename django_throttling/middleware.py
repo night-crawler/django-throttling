b10b65812aacf216dde69fd0e95ccf388ac0401f
vitalii omelchuk <vitaly.omelchuk@gmail.com>
2024-01-21 14:13:52 +0200

    make in compatible with django >= 3.2

--- django_throttling/middleware.py
+++ django_throttling/middleware.py
@@ -1,8 +1,10 @@
 ﻿# -*- coding: utf-8 -*-
+from django.utils.deprecation import MiddlewareMixin
+
 from .util import Throttle
 
-class ThrottleMiddleware(object):
+
+class ThrottleMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
         return Throttle(request, view_func, view_args, view_kwargs).check()
-        #return throttle_check(request, view_func, view_args, view_kwargs)
-
+        # return throttle_check(request, view_func, view_args, view_kwargs)
